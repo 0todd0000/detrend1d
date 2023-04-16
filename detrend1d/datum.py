4d41fe143dc75833badeb1dfec3edf683d4253ba
Todd Pataky <0todd0@gmail.com>
2023-04-16 12:37:02 +0900

    Add NullDatum

--- detrend1d/datum.py
+++ detrend1d/datum.py
@@ -11,6 +11,16 @@ class Datum(TimeSeries):
 		obj = self.interp_durn(x)
 		super().__init__(obj.t, obj.y)
 
+class NullDatum(Datum):
+	def __init__(self, t):
+		t  = np.asarray(t, dtype=float)
+		y  = np.zeros( t.size )
+		super().__init__(t=t, y=y)
+		
+	def set_durn(self, x):
+		t  = np.linspace(self.t0, self.t1, x)
+		return NullDatum( t )
+
 	
 class _ExperimentalDatum( Datum ):
 	
