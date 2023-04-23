cb5abbc3a93b42c665e5af46e7656a62a5d95c1a
Todd Pataky <0todd0@gmail.com>
2023-04-23 12:31:56 +0900

    Remove unneeded intra-cycle trend classes

--- detrend1d/__init__.py
+++ detrend1d/__init__.py
@@ -2,7 +2,6 @@
 __version__ = '0.3.0'  # 2023-04-20
 
 from . import trends
-# from . fns import detrend, detrend_intracycle
 from . import reg
 
 
--- detrend1d/trends/__init__.py
+++ detrend1d/trends/__init__.py
@@ -2,29 +2,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
 # from . import inter, intra
-from . inter import *
-from . intra import *
+from . trends import Linear, LinearFixedIntercept
 
 
 
 
-class Compound(object):
-	def __init__(self, inter=None, intra=None):
-		self.trend0  = inter
-		self.trend1  = intra
-		
-	def apply(self, t, y):
-		y1 = self.trend0.apply(t, y)
-		return self.trend1.apply(t, y1)
 
-	def plot(self, ax=None, t0=0, t1=10, n=51):
-		ax    = plt.gca() if (ax is None) else ax
-		t     = np.linspace(t0, t1, n)
-		y     = self.apply(t, np.zeros(n))
-		ax.plot(t, y)	
-		
-		
-		
 def str2trend(s):
 	s = str(s).lower()
 	if s == 'linear':
